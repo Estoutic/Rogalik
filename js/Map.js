ae64b0f7af0c355dd95bd677c0ade1a10b02c860
Ilya udalov <ilya_udalov_04@mail.ru>
2023-11-10 22:21:03 +0300

    Added fill function to populate the matrix with rooms

--- js/Map.js
+++ js/Map.js
@@ -12,25 +12,38 @@ for (let i = 0; i < rows; i++) {
     }
 }
 
-function fill(map) {
+var rooms = getRandomVal(5, 10);
 
-    var rooms = getRandomVal(5, 10);
+for (let index = 0; index < rooms; index++) {
+    var width = getRandomVal(3, 8);
+    var height = getRandomVal(3, 8);
+    var randomX = getRandomVal(0, columns - width);
+    var randomY = getRandomVal(0, rows - height); 
 
-    while (rooms >= 0) {
-        var width = getRandomVal(3, 8);
-        var height = getRandomVal(3, 8);
-        var randomX = getRandomVal(0, columns);
-        var randomY = getRandomVal(0, rows);
+    var overlaps = false;
+    for (var i = randomY; i < randomY + height; i++) {
+        for (var j = randomX; j < randomX + width; j++) {
+            if (map[i][j].type !== null) {
+                overlaps = true;
+                break;
+            }
+        }
+        if (overlaps) {
+            break;
+        }
+    }
 
-        // for (let i =)
-
- 
+    for (var i = randomY; i < randomY + height; i++) {
+        for (var j = randomX; j < randomX + width; j++) {
+            map[i][j].type = null;
+        }
     }
 }
 
 
+
 function getRandomVal(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
+}
 export { map };
